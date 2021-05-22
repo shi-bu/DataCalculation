721f7fe2d1e777d1ed237b8d517ff2ffbf25b5f4
澁谷拓茉 <shibuya@shibuyatakunoMacBook-Air.local>
2021-05-23 04:45:27 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/DatacalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacalculationApplication.class, args);
+	}
+
+}
